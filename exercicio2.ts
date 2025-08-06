b3cc47af962f0b54b47a30eb3ce6ee39763bbfc1
Anderson Santana <anderslash@gmail.com>
2025-08-06 09:20:39 -0300

    Exercicio 2

--- exercicio2.ts
+++ exercicio2.ts
@@ -1,7 +1,27 @@
 // Crie um array chamado products que contenha objetos, cada um seguindo uma interface Product (com id, name, price, category). Defina a interface Product.
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+};
+
+const products: Product[] = [
+  { id: 1, name: "Laptop", price: 1200, category: "Eletrônicos" },
+  { id: 2, name: "Mouse", price: 25, category: "Eletrônicos" },
+  { id: 3, name: "Livro TypeScript", price: 50, category: "Livros" },
+  { id: 4, name: "Teclado", price: 75, category: "Eletrônicos" },
+  { id: 5, name: "Caneta", price: 2, category: "Escritório" },
+];
+
 
 // Use o método map no array products para criar um novo array contendo apenas os nomes dos produtos (um array de strings). 
+const productNames: string[] = products.map(product => product.name);
 
 // Use o método filter no array products para criar um novo array contendo apenas os produtos de uma categoria específica. 
+const electronicProducts: Product[] = products.filter(product => product.category === "Eletrônicos");
+console.log("Produtos eletrônicos:", electronicProducts);
 
 // Imprima os resultados de map, filter no console.
+console.log("Nomes dos produtos:", productNames);
+console.log("Produtos eletrônicos:", electronicProducts);
